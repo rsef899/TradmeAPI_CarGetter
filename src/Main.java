05315b32922f034909876b481f5bab22ac7ea41f
rsef899 <rsef899@aucklanduni.ac.nz>
2023-06-04 01:58:28 +1200

    Read the Authentication keys

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.JSONObject;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		readKeys();
 
+	}
+
+	public static JSONObject readKeys() {
+		String jsonFilePath = "C:\\Users\\schoo\\OneDrive\\Documents\\Other\\Projects\\TradmeAPI_CarGetter\\src\\keys.json";
+
+		try {
+			FileReader fr = new FileReader(jsonFilePath);
+
+			BufferedReader bufferedReader = new BufferedReader(fr);
+			StringBuilder jsonKeyString = new StringBuilder();
+			String line;
+
+			// get the JSON string
+			while ((line = bufferedReader.readLine()) != null) {
+				jsonKeyString.append(line);
+			}
+			bufferedReader.close();
+
+			// convert to a JSON object
+			JSONObject keys = new JSONObject(jsonKeyString.toString());
+
+			return keys;
+
+		} catch (IOException e) {
+			// TODO: Handle no key exception
+			System.out.println("No Keys Found!");
+			return null;
+		}
 	}
 
 }
